f8859f3d91ff9d91f18bc0e7f345e361d71c2ca9
MDAAdvisors <MDAAdvisors@MDAAdvisors-HP>
2014-12-15 11:15:13 -0500

    3rd of the morning

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(int argc, char** argv)
     key_t ttt =ftok("C:\\cygwin\\home\\MDAAdvisors\\pa6\\pa6", 11);
     printf("\n%i\n",ttt);
     // this line added at 10:30
+    // this line added at 11:20
     return (EXIT_SUCCESS);
 }
 
