2a0def218e46d60058b7b3ddc00d995ff22b7902
MDAAdvisors <MDAAdvisors@MDAAdvisors-HP>
2014-12-15 11:25:54 -0500

    4th of the morning

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
 {
     //key_t ftok(const char *pathname, int proj_id);
     key_t ttt =ftok("C:\\cygwin\\home\\MDAAdvisors\\pa6\\pa6", 11);
-    printf("\n%i\n",ttt);
+    printf("\nreturn value: %i\n",ttt);
     // this line added at 10:30
     // this line added at 11:20
     return (EXIT_SUCCESS);
